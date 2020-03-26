e7368556840b587d5d74b5db5850cbca63813a60
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-26 21:57:12 +0000

    Deploy elliotleelewis/UniY2-ProfessionalDevelopment to github.com/elliotleelewis/UniY2-ProfessionalDevelopment.git:gh-pages

---
+++ precache-manifest.c2e9642e652dabb520b7676f2dbe3853.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93c4b9a356fc9ea20892ae60d9d8151",
+    "url": "/UniY2-ProfessionalDevelopment/index.html"
+  },
+  {
+    "revision": "23e61a587af4a597705e",
+    "url": "/UniY2-ProfessionalDevelopment/static/css/main.fd5fb3d5.chunk.css"
+  },
+  {
+    "revision": "5e0ac48d7a5f8fab3ebf",
+    "url": "/UniY2-ProfessionalDevelopment/static/js/2.9efea6df.chunk.js"
+  },
+  {
+    "revision": "23e61a587af4a597705e",
+    "url": "/UniY2-ProfessionalDevelopment/static/js/main.40b36c31.chunk.js"
+  },
+  {
+    "revision": "b3d382b3e236e7908810",
+    "url": "/UniY2-ProfessionalDevelopment/static/js/runtime-main.57231b53.js"
+  }
+]);
